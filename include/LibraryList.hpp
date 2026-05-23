810ad95c51076d2c8e26a1972173f4fabbf39cf9
NikiZip <nikitu0008@gmail.com>
2026-05-23 19:32:20 +0300

    Create LibraryList.hpp

---
+++ include/LibraryList.hpp
@@ -0,0 +1,23 @@
+#ifndef LIBRARY_LIST_HPP
+#define LIBRARY_LIST_HPP
+
+#include <string>
+#include <vector>
+
+inline const std::vector<std::string> library = {
+    "<algorithm>","<array>","<atomic>","<barrier>","<bitset>","<bit>","<chrono>","<complex>",
+    "<concepts>","<condition_variable>","<coroutine>","<deque>","<exception>","<execution>",
+    "<forward_list>","<format>","<fstream>","<functional>","<future>","<initializer_list>",
+    "<iomanip>","<ios>","<iosfwd>","<iostream>","<istream>","<iterator>","<latch>","<limits>",
+    "<list>","<locale>","<map>","<memory>","<memory_resource>","<mutex>","<new>","<numeric>",
+    "<optional>","<ostream>","<queue>","<random>","<ranges>","<ratio>","<regex>",
+    "<scoped_allocator>","<set>","<shared_mutex>","<sstream>","<stack>","<stdexcept>",
+    "<streambuf>","<stop_token>","<string>","<string_view>","<system_error>","<thread>",
+    "<tuple>","<type_traits>","<typeindex>","<typeinfo>","<unordered_map>","<unordered_set>",
+    "<utility>","<valarray>","<variant>","<vector>","<version>","<charconv>","<compare>",
+    "<cassert>","<cctype>","<cerrno>","<cfenv>","<cfloat>","<cinttypes>","<ciso646>",
+    "<climits>","<clocale>","<cmath>","<cstdbool>","<cstddef>","<cstdint>","<cstdio>",
+    "<cstdlib>","<cstring>","<ctgmath>","<cuchar>","<cwchar>","<cwctype>","<ccomplex>"
+};
+
+#endif // LIBRARY_LIST_HPP
